d22bbe14cec0a11df5ee6e23f93ab9c645c00dd9
arsklx <aakhmetov088@gmail.com>
2024-03-19 18:07:28 +0600

    FINISHED

--- LAb7/pygames_2.py
+++ LAb7/pygames_2.py
@@ -1,16 +1,45 @@
-import pygame 
-pygame.init()
-screen = pygame.display.set_mode((1920, 1000))
-done  = True
-image = pygame.image.load("mickeyclock.jpeg")
-angle = 180
+import pygame
+import datetime
 
-while done:
-    image = pygame.image.load("mickeyclock.jpeg")
-    screen.fill((0, 0, 0))
+pygame.init()
+height = 600
+width = 600
+
+sc = pygame.display.set_mode((height , width))
+
+def rot_center(image, angle, x, y):
+    
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center = image.get_rect(center = (x, y)).center)
+
+    return rotated_image, new_rect
+
+background = pygame.image.load('mainclock.png') 
+right = pygame.image.load('rightarm.png')
+left = pygame.image.load('leftarm.png')
+
+background = pygame.transform.scale(background , (600 , 600))
+right = pygame.transform.scale(right , (600 , 600))
+left = pygame.transform.scale(left , (50 , 300))
+left = pygame.transform.flip(left , 180 , 180)
+
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done = False
-    rotate = pygame.transform.rotate(image, angle)
-    screen.blit(rotate, (0, 0))
-    pygame.display.flip()
+            pygame.quit()
+            
+    sec = datetime.datetime.now().second
+    min = datetime.datetime.now().minute
+    
+    x = (-6*min)%360
+    y = ((-1)*sec*6)%360
+    
+    rot_right , x = rot_center(right , x , 300 , 300)
+    rot_left , y = rot_center(left , y , 300 , 300)
+    
+    sc.blit(background, (0 , 0))
+    sc.blit(rot_right , x)
+    sc.blit(rot_left , y)
+    
+    pygame.display.update()
